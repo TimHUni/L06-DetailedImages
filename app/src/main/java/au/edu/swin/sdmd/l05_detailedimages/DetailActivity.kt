b91f56ecbb105a68a2302dfcd2307f7d2c351c9b
Exoreath <102925930@student.swin.edu.au>
2022-09-19 16:56:47 +1000

    Updated code to be up to date with lecture video

--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
@@ -2,11 +2,21 @@ package au.edu.swin.sdmd.l05_detailedimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        val location = intent.getParcelableExtra<Location>("location")
+
+        location?.let {
+            val vName = findViewById<TextView>(R.id.name)
+            vName.text = it.name
+
+            val vAuthor = findViewById<TextView>(R.id.author)
+            vAuthor.text = it.author
+        }
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/Location.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/Location.kt
@@ -1,6 +1,10 @@
 package au.edu.swin.sdmd.l05_detailedimages
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Location(val name: String, val author: String,
                     val latitude: Float, val longitude: Float,
-                    val visited: Boolean = false) {
+                    val visited: Boolean = false): Parcelable {
 }
